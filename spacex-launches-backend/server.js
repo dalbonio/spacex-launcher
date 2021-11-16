f9df7de4beb480d07c17abc1fbbe3539744a6eb7
Lucas Dalbonio <dalbonio2@gmail.com>
2021-11-15 21:43:09 -0300

    cors config

--- spacex-launches-backend/server.js
+++ spacex-launches-backend/server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+var corsOptions = {
+	origin: ['http://localhost:3000', 'localhost:3000', '127.0.0.1:3000'],
+}
+
+app.use(cors(corsOptions))
 require('./src/routes/index')(app);
- 
-app.use(cors());
+
+
 app.use(express.json());
 app.listen(3001);
 
